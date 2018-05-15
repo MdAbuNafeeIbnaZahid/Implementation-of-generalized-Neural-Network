1218b61a1345b9e3374c195b6a80d5777be06d11
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2018-05-15 09:02:15 +0600

    uploaded to moodle. code is buggy. Giving all the estimates at a round

---
+++ for upload to moodle/1305023/neural_network/logistic_func.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+
+a = 1
+
+def getF(x):
+    denominator = 1 + np.power( math.e, (-a * x) )
+    return 1/denominator;
+
+
+
+def getFPrime(x):
+    return  a * getF(x) * (1 - getF(x) )
